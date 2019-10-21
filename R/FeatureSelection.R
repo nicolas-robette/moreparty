30932b658d3cd872c5eed81a037834c2fea4c90f
nicolas-robette <nicolas.robette@free.fr>
2019-10-21 14:47:33 +0200

    Update for feature selection

--- R/FeatureSelection.R
+++ R/FeatureSelection.R
@@ -1,75 +1,18 @@
 # @import party
 #
-#' @importFrom stats predict
-#'
+# @importFrom stats predict
+#
 #' @export
 
-
-FeatureSelection <- function(Y, X, recompute = F, ntree = 3000, measure = NULL, parallel = FALSE, ...) {
-
-  if(class(Y)=='factor') mtry <- ceiling(sqrt(ncol(X))) # automatically set mtry to sqrt(p)
-  if(class(Y)=='numeric') mtry <- ceiling(ncol(X)/3) # automatically set mtry to p/3
-
-  dat <- cbind(Y, X) # create the data
-  names(dat) <- c("response", paste("V", 1:ncol(X), sep = ""))
-
-  if(is.null(measure)) {
-    #vi_fct <- function(x,measure) { varImp::varImp(x) }
-    vi_fct <- function(x,measure) { fastvarImp(x, parallel=parallel) }
-  } else if(measure=="AUC") {
-    #vi_fct <- function(x,measure) { -varImp::varImpAUC(x) }
-    vi_fct <- function(x,measure) { -fastvarImpAUC(x, parallel=parallel) }
-  } else {
-    #vi_fct <- function(x,measure) { varImp::varImp(x, measure=measure) }
-    vi_fct <- function(x,measure) { fastvarImp(x, measure=measure, parallel=parallel, ...) }
+FeatureSelection <- function(Y, X, method="NRFE", ntree = 1000, measure = NULL, nperm = 30, alpha = 0.05, distrib = 'approx', parallel = FALSE, ...) {
+  if(method=='NRFE') {
+    res <- NRFE(Y, X, measure=measure, ntree=ntree, parallel=parallel)  
+  } else if(method=='RFE') {
+    res <- RFE(Y, X, measure=measure, ntree=ntree, parallel=parallel)  
+  } else if(method=='ALT') {
+    res <- ALT(Y, X, measure=measure, ntree=ntree, nperm=nperm, alpha=alpha, parallel=parallel)  
+  } else if(method=='HAPF') {
+    res <- HAPF(Y, X, measure=measure, ntree=ntree, nperm=nperm, alpha=alpha, parallel=parallel)  
   }
-
-  forest <- party::cforest(response ~ ., data = dat, # fit a forest
-                          controls = party::cforest_unbiased(mtry = mtry, ntree = ntree))
-  selections <- list() # a list that contains the sequence of selected variables
-  selections[[ncol(X)]] <- names(sort(vi_fct(forest, measure), decreasing = T))
-  errors <- c()
-
-  for (i in ncol(X):1) { # take backward rejection steps
-    print(paste('steps to go =',i))
-    if(class(Y)=='factor') mtry <- ceiling(sqrt(i)) # set mtry to sqrt() of remaining variables
-    if(class(Y)=='numeric') mtry <- ceiling(i/3) # set mtry to ()/3 of remaining variables
-    forest <- party::cforest(as.formula(paste("response", paste(selections[[i]],
-                                                         collapse = " + "), sep = " ~ ")), data = dat, # fit forest
-                             controls = party::cforest_unbiased(mtry = mtry, ntree = ntree))
-    errors[i] <- mean((as.numeric(Y) - # compute the OOB-error
-                         as.numeric(predict(forest, OOB = T)))^2)
-    # define the next set of variables
-    if (recompute == F & i > 1) selections[[i - 1]] <- selections[[i]][-i]
-    if (recompute == T & i > 1) selections[[i - 1]] <- names(sort(vi_fct(forest, measure), decreasing = T))[-i]
-  }
-
-  # compute the error expected when no predictor is used at all
-  errors <- c(mean((as.numeric(Y) - ifelse(length(unique(Y)==2),round(mean(as.numeric(Y))), mean(Y)) )^2 ),
-              errors)
-
-  # define the number of variables determined by the 0 s.e. and 1 s.e. rule
-  optimum.number.0se <- which.min(errors)
-  optimum.number.1se <- which(errors <= min(errors) + 1 * ifelse(all(Y %in% 0:1),
-                                                                 sqrt(min(errors) * (1 - min(errors)) / nrow(X)), 0))[1]
-  # compute the corresponding forests and OOB-errors
-  if (optimum.number.0se == 1) {forest.0se <- c(); selection.0se <- c()}
-  if (optimum.number.1se == 1) {forest.1se <- c(); selection.1se <- c()}
-  if (optimum.number.0se != 1) {
-    selection.0se <- selections[[optimum.number.0se - 1]]
-    forest.0se <- party::cforest(as.formula(paste("response", paste(selection.0se,
-                                                             collapse = " + "), sep = " ~ ")), data = dat,
-                                 controls = party::cforest_unbiased(mtry = mtry, ntree = ntree))}
-  if (optimum.number.1se != 1) {
-    selection.1se <- selections[[optimum.number.1se - 1]]
-    forest.1se <- party::cforest(as.formula(paste("response", paste(selection.1se,
-                                                             collapse = " + "), sep = " ~ ")), data = dat,
-                                 controls = party::cforest_unbiased(mtry = mtry, ntree = ntree))}
-  oob.error.0se <- errors[optimum.number.0se]
-  oob.error.1se <- errors[optimum.number.1se]
-  selection.0se <- names(X)[as.numeric(gsub('V','',selection.0se))]
-  selection.1se <- names(X)[as.numeric(gsub('V','',selection.1se))]
-  return(list("selection.0se" = selection.0se, "forest.0se" = forest.0se,
-              "oob.error.0se" = oob.error.0se, "selection.1se" = selection.1se,
-              "forest.1se" = forest.1se, "oob.error.1se" = oob.error.1se))
+  return(res)
 }
--- README.md
+++ README.md
@@ -3,11 +3,12 @@
 ## an R package for conditional inference random forests
 
 This package aims at complementing the party package, with parallelization and interpretation tools.
+
 It provides functions for :
 
 * parallelized conditional random forest
 * parallelized variable importance
-* recursive and non-recursive feature elimination algorithm
+* feature selection : recursive and non-recursive feature elimination, algorithms based on permutation tests
 * surrogate tree
 * bivariate association measures
 * prototypes
@@ -29,16 +30,18 @@ install_github("nicolas-robette/moreparty")
 
 ## References
 
-Gregorutti B., B. Michel B., and Saint Pierre P. "Correlation and variable importance in random forests". arXiv:1310.5726, 2017.
+Altmann A., Toloşi L., Sander O., and Lengauer T.. "Permutation importance: a corrected feature importance measure". *Bioinformatics*, 26(10):1340-1347, 2010.
+
+Gregorutti B., Michel B., and Saint Pierre P. "Correlation and variable importance in random forests". arXiv:1310.5726, 2017.
 
 Hapfelmeier A. and Ulm K. "A new variable selection approach using random forests". *Computational Statistics and Data Analysis*, 60:50–69, 2013.
 
-Hothorn T, Hornik K, Van De Wiel MA, Zeileis A. "A lego system for conditional inference". *The American Statistician*. 60:257–263, 2006.
+Hothorn T., Hornik K., Van De Wiel M.A., Zeileis A. "A lego system for conditional inference". *The American Statistician*. 60:257–263, 2006.
 
-Hothorn T, Hornik K, Zeileis A. "Unbiased Recursive Partitioning: A Conditional Inference Framework". *Journal of Computational and Graphical Statistics*, 15(3):651-674, 2006.
+Hothorn T., Hornik K., Zeileis A. "Unbiased Recursive Partitioning: A Conditional Inference Framework". *Journal of Computational and Graphical Statistics*, 15(3):651-674, 2006.
 
-Molnar, Christoph. *Interpretable machine learning. A Guide for Making Black Box Models Explainable*, 2019.
+Molnar, C. *Interpretable machine learning. A Guide for Making Black Box Models Explainable*, 2019.
 [https://christophm.github.io/interpretable-ml-book/]
 
-Strobl, C., Malley, J., and Tutz, G. "An Introduction to Recursive Partitioning: Rationale, Application, and Characteristics of Classication and Regression Trees, Bagging, and Random Forests". *Psychological methods*, 14(4):323-348, 2009.
+Strobl, C., Malley, J., and Tutz, G. "An Introduction to Recursive Partitioning: Rationale, Application, and Characteristics of Classification and Regression Trees, Bagging, and Random Forests". *Psychological methods*, 14(4):323-348, 2009.
 

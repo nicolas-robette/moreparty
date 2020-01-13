8e74a10bedfeb43a100966ed69fdfe192bbe2a0a
nicolas-robette <nicolas.robette@free.fr>
2020-01-13 17:36:36 +0100

    GetSplitStats function now works for partykit trees as well

--- R/GetSplitStats.R
+++ R/GetSplitStats.R
@@ -1,10 +1,18 @@
 #' @export
 
 GetSplitStats <- function(ct) {
-  ternodes <- unique(where(ct))
-  allnodes <- 1:max(ternodes)
-  splnodes <- setdiff(allnodes,ternodes)
-  stats <- lapply(splnodes, function(x) round(rev(sort(log(nodes(ct,x)[[1]]$criterion$criterion))),10))
-  names(stats) <- splnodes
+  if(class(ct)[1]=='BinaryTree') {
+    ternodes <- unique(where(ct))
+    allnodes <- 1:max(ternodes)
+    splnodes <- setdiff(allnodes,ternodes)
+    stats <- lapply(splnodes, function(x) round(rev(sort(log(nodes(ct,x)[[1]]$criterion$criterion))),10))
+    names(stats) <- splnodes
+  }
+  if(class(ct)[1]=='constparty') {
+    allnodes <- partykit::nodeids(ct, terminal = FALSE)
+    ternodes <- partykit::nodeids(ct, terminal = TRUE)
+    splnodes <- setdiff(allnodes,ternodes)
+    stats <- partykit::nodeapply(ct, ids = splnodes, FUN = function(x) partykit::info_node(x)$criterion)
+  }
   return(stats)
 }

5f9312c35065f9f1b56243c37bc95addeb734813
Nicolas <nicolas.robette@free.fr>
2023-11-21 13:23:16 +0100

    new summary element

--- R/GetSplitStats.R
+++ R/GetSplitStats.R
@@ -1,24 +1,29 @@
 #' @export
 
 GetSplitStats <- function(ct) {
-  if(class(ct)[1]=='BinaryTree') {
-    ternodes <- unique(party::where(ct))
-    allnodes <- 1:max(ternodes)
-    splnodes <- setdiff(allnodes,ternodes)
-    stats <- lapply(splnodes, function(x) round(rev(sort(log(party::nodes(ct,x)[[1]]$criterion$criterion))),10))
-    names(stats) <- splnodes
+
+  if(class(ct)[1]!="constparty") stop("ctree is not of class constparty from partykit package")
+  
+  allnodes <- partykit::nodeids(ct, terminal = FALSE)
+  ternodes <- partykit::nodeids(ct, terminal = TRUE)
+  splnodes <- setdiff(allnodes,ternodes)
+  stats <- partykit::nodeapply(ct, ids = splnodes, FUN = function(x) partykit::info_node(x)$criterion)
+  for(i in 1:length(stats)) {
+    z <- as.data.frame(t(stats[[i]]))
+    z <- z[order(-z$criterion),]
+    z$ratio <- z$criterion/max(z$criterion)
+    stats[[i]] <- z
   }
-  if(class(ct)[1]=='constparty') {
-    allnodes <- partykit::nodeids(ct, terminal = FALSE)
-    ternodes <- partykit::nodeids(ct, terminal = TRUE)
-    splnodes <- setdiff(allnodes,ternodes)
-    stats <- partykit::nodeapply(ct, ids = splnodes, FUN = function(x) partykit::info_node(x)$criterion)
-    for(i in 1:length(stats)) {
-      z <- as.data.frame(t(stats[[i]]))
-      z <- z[order(-z$criterion),]
-      z$ratio <- z$criterion/max(z$criterion)
-      stats[[i]] <- z
-    }
-  }
-  return(stats)
+
+  su <- do.call("rbind.data.frame", lapply(stats, function(x) c(rownames(x)[1:2],x$ratio[2])))
+  su <- data.frame(names(stats), su)
+  names(su) <- c("node", "split_var", "best_var", "temp")
+  su$temp <- as.numeric(su$temp)
+  su$ratio <- character(length = nrow(su))
+  su$ratio[su$temp <= 1000] <- as.character(round(su$temp[su$temp <= 1000], 2))
+  su$ratio[su$temp > 1000] <- formatC(su$temp[su$temp > 1000], format="e", digits=1)
+  su$temp <- NULL
+  
+  return(list(details = stats,
+              summary = su))
 }
